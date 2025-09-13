85f19ce4534e9dd7ac6c9e6266bac5ea1a5ffaef
Ishaanz-pro <ishaanzalani18@gmail.com>
2025-09-13 17:21:22 +0530

    Day 4: Connected Flask frontend to FastAPI backend

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
 @app.route("/law-guide")
 def law_guide():
-    return "<h2>Cyber Law Guide will come here soon 🚀</h2>"
+    try:
+        # Call FastAPI backend
+        response = requests.get("http://127.0.0.1:8000")
+        data = response.json()
+    except Exception as e:
+        data = {"message": f"Error connecting to backend: {str(e)}"}
+    
+    return render_template("law_guide.html", data=data)
 
 if __name__ == "__main__":
     app.run(debug=True)
-
